b873a7af4012036fdf76fec861fedaaad19041a0
Corbiezorq <jee.c@infonie.fr>
2016-09-23 13:41:10 +0200

    creation table bdd

---
+++ laravel/database/migrations/2016_09_23_112247_create_user_tags_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('tag_id')->references('id')->on('tags');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_tags');
+    }
+}
