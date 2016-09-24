d87c051a46deaf101e13a7f9582d5e10de40bbaa
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-09-24 17:00:04 +0200

    Added error display, updated menu with simple styles

--- laravel/resources/views/layouts/simple.blade.php
+++ laravel/resources/views/layouts/simple.blade.php
@@ -14,33 +14,51 @@
         <link rel="stylesheet" href="css/normalize.min.css">
         <link rel="stylesheet" href="css/main.css">
 
+        <style>
+            #menu{list-style-type: none; border-bottom:1px solid #333; padding: 5px 0; font-size:0.8em}
+            #menu li{display: inline; padding-right: 10px;}
+            #menu li.title{border-left:3px solid #333; padding-left:3px;}
+        </style>
+
     </head>
     <body>
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <ul id="menu" style="list-style-type: none; border-bottom:1px solid #333; padding: 5px 0; font-size:0.8em">
-            <li style="display: inline; padding-right: 10px;">{{ link_to('/', 'Accueil') }}</li>
-            <li style="display: inline; padding-right: 10px; border-left:3px solid #333; padding-left:3px;">Users</li>
-            <li style="display: inline; padding-right: 10px;">{{ link_to('/users/add', 'Add') }}</li>
-            <li style="display: inline; padding-right: 10px;">{{ link_to('/users', 'Index') }}</li>
-            <li style="display: inline; padding-right: 10px;">{{ link_to('/users/register', 'Register') }}</li>
-            <li style="display: inline; padding-right: 10px;">{{ link_to('/users/login', 'Login') }}</li>
-            <li style="display: inline; padding-right: 10px; border-left:3px solid #333; padding-left:3px;">Photos</li>
-            <li style="display: inline; padding-right: 10px;">{{ link_to('/photos/add', 'Add') }}</li>
-            <li style="display: inline; padding-right: 10px;">{{ link_to('/photos/index', 'Index') }}</li>
-            <li style="display: inline; padding-right: 10px; border-left:3px solid #333; padding-left:3px;">Roles</li>
-            <li style="display: inline; padding-right: 10px;">{{ link_to('/roles/add', 'Add') }}</li>
-            <li style="display: inline; padding-right: 10px;">{{ link_to('/roles/index', 'Index') }}</li>
+        <ul id="menu" style="">
+            <li>{{ link_to('/', 'Accueil') }}</li>
+            <li>Users</li>
+            <li>{{ link_to('/users/create', 'Add') }}</li>
+            <li>{{ link_to('/users', 'Index') }}</li>
+            <li>{{ link_to('/users/login_register', 'Register') }}</li>
+            <li>{{ link_to('/users/login', 'Login') }}</li>
+            <li class="title">Photos</li>
+            <li>{{ link_to('/photos/add', 'Add') }}</li>
+            <li>{{ link_to('/photos/index', 'Index') }}</li>
+            <li class="title">Roles</li>
+            <li>{{ link_to('/roles/add', 'Add') }}</li>
+            <li>{{ link_to('/roles/index', 'Index') }}</li>
 
         </ul>
 
         <h1>{{ isset($pageTitle) ? $pageTitle : 'Une page...' }}</h1>
 
+		<!-- Errors -->
+		@if (count($errors) > 0)
+		  <ul>
+			  @foreach ($errors->all() as $error)
+			  <li>{{ $error }}</li>
+			  @endforeach
+		  </ul>
+		@endif
+		<!-- /Errors -->
+		
+		<!-- Content -->
         <div id="content">
             @yield('content')
         </div>
+		<!-- /Content -->
 
         <script src="js/vendor/jquery.min.js"></script>
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
