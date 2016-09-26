08c83594110c79d42766f2ca85ab355ac61b9510
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-09-26 18:21:28 +0200

    Updated routes

--- laravel/routes/web-mt.php
+++ laravel/routes/web-mt.php
@@ -1,4 +1,20 @@
 <?php
-// users
-Route::get('user/add', 'UserController@add_form');
-Route::post('user/add', 'UserController@add');
+/*
+ * User's routes
+ */
+// Index
+Route::get('/users', 'UserController@index');
+// Add user
+Route::get('/user/create', 'UserController@create');
+Route::post('/user/store', 'UserController@store');
+// View
+Route::get('/user/{id}', 'UserController@view');
+// Destroy
+Route::get('/user/{id}/destroy', 'UserController@destroy');
+// Edit
+Route::get('/user/{id}/edit', 'UserController@edit');
+Route::post('user/{id}/update', 'UserController@update');
+// Login/Register
+Route::get('/login', 'UserController@login_register');
+Route::post('/register', 'UserController@register');
+Route::post('/login', 'UserController@login');
