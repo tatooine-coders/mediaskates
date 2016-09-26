111d0bcb56efb74096f0c9d304adf0b156893767
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-09-27 00:51:42 +0200

    Added model use, removed useless actions and controllers

--- laravel/app/Http/Controllers/User/DisciplineController.php
+++ laravel/app/Http/Controllers/User/DisciplineController.php
@@ -3,84 +3,85 @@
 namespace App\Http\User\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use App\Discipline;
 
 class DisciplineController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+	//
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+	//
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+	//
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+	//
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+	//
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+	//
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+	//
+  }
 }
--- laravel/app/Http/Controllers/User/UserController.php
+++ laravel/app/Http/Controllers/User/UserController.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * User Controller
- * 
- * Maintainer: mtancoigne
- */
 
 namespace App\Http\User\Controllers;
 
@@ -13,21 +8,6 @@ use App\User;
 
 class UserController extends Controller
 {
-
-  /**
-   * Displays the list of users
-   *
-   * @return Illuminate\Http\Response
-   */
-  public function index()
-  {
-	$users = User::query()->get();
-
-	return view('users/index', [
-		'pageTitle' => 'Liste des utilisateurs',
-		'users' => $users
-	]);
-  }
 
   /**
    * Displays an user profile.
@@ -47,35 +27,11 @@ class UserController extends Controller
   }
 
   /**
-   * Creates a new user in the DB
-   * 
-   * @param Request $request
+   * Logs an user out
    *
    * @return Illuminate\Http\Response
    */
-  public function register(Request $request)
-  {
-	
-  }
-
-  /**
-   * Logs an user in the system
-   *
-   * @param Request $request
-   * 
-   * @return Illuminate\Http\Response
-   */
-  public function login(Request $request)
-  {
-	
-  }
-
-  /**
-   * Displays to form to login/register
-   *
-   * @return Illuminate\Http\Response
-   */
-  public function login_register()
+  public function logout()
   {
 	
   }
@@ -90,48 +46,6 @@ class UserController extends Controller
   public function destroy(int $id)
   {
 	
-  }
-
-  /**
-   * Displays the form to add an user. (Admin mode)
-   *
-   * @return Illuminate\Http\Response
-   */
-  public function create()
-  {
-	// Fetch the roles (for the select element)
-	$roles = \App\Role::query()->pluck('name', 'id');
-
-	return view('users/create', [
-		'pageTitle' => 'Ajouter un utilisateur',
-		'roles' => $roles,
-	]);
-  }
-
-  /**
-   * Saves a new user in DB (Admin mode !)
-   *
-   * @return Illuminate\Http\Response
-   */
-  public function store(Request $request)
-  {
-	// Data validation (https://laravel.com/docs/5.3/validation)
-	$this->validate($request, [
-		'first_name' => 'required',
-		'last_name' => 'required',
-		'pseudo' => 'bail|required|unique:users',
-		'email' => 'bail|required|email|unique:users',
-		'password' => 'required',
-		'password2' => 'required',
-	]);
-
-	$data = $request->all();
-
-	$user = User::create($data);
-
-	// Redirection et message
-	\Session::flash('message', 'Nouvel utilisateur créé');
-	return Redirect::to('users/index');
   }
 
   /**
@@ -164,7 +78,7 @@ class UserController extends Controller
   public function update(int $id, Request $request)
   {
 	$user = User::findOrFail($id);
-	
+
 	$this->validate($request, [
 		'first_name' => 'required',
 		'last_name' => 'required',
@@ -177,5 +91,40 @@ class UserController extends Controller
 	// Redirection et message
 	\Session::flash('message', 'Utilisateur mis à jour !');
 	return \Redirect::to('user/' . $id);
+  }
+
+  /**
+   * Form to edit user preferences
+   * @todo Merge with edit ?
+   * 
+   * @return Illuminate\Http\Response
+   */
+  public function edit_prefs()
+  {
+	
+  }
+
+  /**
+   * Saves the preferences in DB
+   * 
+   * @param Request $request
+   * 
+   * @return Illuminate\Http\Response
+   */
+  public function update_prefs(Request $request)
+  {
+	
+  }
+
+  /**
+   * Updates password in db
+   * 
+   * @param Request $request
+   * 
+   * @return Illuminate\Http\Response
+   */
+  public function update_passwd(Request $request)
+  {
+	
   }
 }
