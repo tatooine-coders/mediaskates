d9043f83ca50a97a7a4cf9000bb3ea4b6de23d19
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-10-13 00:02:50 +0200

    Added PHPCS & PHPUnit to dependencies. Created a small script to run them all.

--- laravel/codesniff.sh
+++ laravel/codesniff.sh
@@ -1,2 +1,20 @@
 #!/bin/bash -
-phpcs --standard=PSR2 app/
+
+### CodeSniffer
+sh -c "vendor/bin/phpcs -p --standard=PSR2 ./app";
+
+echo -e "\nTo fix some of the issues, you can run:";
+echo "./vendor/bin/php-cs-fixer fix app/ --level=psr2 --verbose --dry-run";
+echo -e "\nAlternatively, you can launch PHPCS in interactive mode, with the '-a' parameter.\n\n";
+
+read -p "Press enter to continue" CONTINUE;
+
+### Mess detector
+#sh -c "vendor/bin/phpmd ./app html cleancode,codesize,controversial,design,naming > MessDetector.html"
+
+#echo -e "\n";
+#read -p "Press enter to continue" CONTINUE;
+
+### PHPUnit
+sh -c "vendor/bin/phpunit"
+
