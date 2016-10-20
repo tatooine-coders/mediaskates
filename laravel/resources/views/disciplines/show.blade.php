227aa1af1da0251440f68d73ee4c67ee4ea7cf0b
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-10-20 09:59:07 +0200

    Prepared discipline.show

--- laravel/resources/views/disciplines/show.blade.php
+++ laravel/resources/views/disciplines/show.blade.php
@@ -1,24 +1,23 @@
 @extends('layouts/simple')
 
 @section('content')
-    <h2>
-        {{{ $discipline->name }}}</h2>
+    <h2>{{ $discipline->name }}</h2>
     <pre>
-        {{{ $discipline->name }}}
-        {{{ $discipline->logo }}}
-
-        <!-- lien pour edit -->
-
-        <a href="{{ route('admin.discipline.edit', $discipline->id) }}" class="btn btn-primary">Edit Discipline</a>
-
-        <!-- form invisble pour delete -->
-
-        {!! Form::model($discipline, [
-        'method' => 'DELETE',
-        'route' => ['admin.discipline.destroy', $discipline->id]
-        ]) !!}
-        {!! Form::submit('Delete') !!}
-        {!! Form::close() !!}
-
+        {{ $discipline->name }}
+        <img src="{{ asset(DISCIPLINES_THUMB_FOLDER.$discipline->logo) }}" alt="{{ $discipline->name}}" />
     </pre>
+
+<hr>
+<h2>Evenements pour cette discipline</h2>
+
+@if(count($discipline->events)>0)
+    @foreach($discipline->events as $event)
+        <div>
+            <a href="{{ route('event.show', $event->id) }}">{{ $event->name }}</a>
+        </div>
+    @endforeach
+@else
+    Pas d'évènements
+@endif
+
 @endsection
