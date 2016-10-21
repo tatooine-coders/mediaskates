9c01ba83c7de6517481497db42d6142c988ab169
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-10-21 15:23:33 +0200

    Finished view

--- laravel/resources/views/admin/events/show.blade.php
+++ laravel/resources/views/admin/events/show.blade.php
@@ -37,13 +37,12 @@
 </div>
 
 <h2>Photos</h2>
-<div class="page-wrapper">
-    <div class="content">
-        @if(count($event->photos)>0)
-        <div class="thumbnails">
+@if(count($event->photos)>0)
+<div class="thumbnails">
+    <div class="grid-5 has-gutter">
         @foreach($event->photos as $photo)
         <div class="thumbnail">
-            <img src="{{ asset(UPLOADS_THUMB_FOLDER.$photo->file) }}" alt=""/>
+            <a href="{{ asset(UPLOADS_PIC_FOLDER.$photo->file) }}" class="fancybox-pic" data-fancybox-group="group1"><img src="{{ asset(UPLOADS_THUMB_FOLDER.$photo->file) }}" alt=""/></a>
             <div class="actions">
                 <a href="{{ route('admin.photo.show', $photo->id) }}" class="btn primary" title="Afficher"><i class="fa fa-fw fa-eye"></i></a>
                 <a href="{{ route('admin.photo.edit', $photo->id) }}" class="btn primary" title="Editer"><i class="fa fa-fw fa-pencil"></i></a>
@@ -55,14 +54,40 @@
             </div>
         </div>
         @endforeach
-        </div>
-        @else
-        <div class="dashboard-empty">
-            <div class="w50 center">
-                Il n'y a aucune photo pour cet évènement pour le moment.
-            </div>
-        </div>
-        @endif
     </div>
 </div>
+<script>
+        $('.fancybox-pic').fancybox({
+            prevEffect: 'none',
+            nextEffect: 'none',
+            closeBtn: true,
+            arrows: true,
+            nextClick: true,
+            helpers: {
+                thumbs: {
+                    width: 50,
+                    height: 50
+                },
+                overlay: {
+                    closeClick: true, // if true, fancyBox will be closed when user clicks on the overlay
+                    speedOut: 200, // duration of fadeOut animation
+                    showEarly: true, // indicates if should be opened immediately or wait until the content is ready
+                    locked: true   // if true, the content will be locked into overlay
+                },
+            }
+        });
+        $('.thumbnails').masonry({
+            itemSelector: '.thumbnail',
+            percentPosition: true
+        });
+</script>
+@else
+<div class="page-wrapper">
+    <div class="dashboard-empty">
+        <div class="w50 center">
+            Il n'y a aucune photo pour cet évènement pour le moment.
+        </div>
+    </div>
+</div>
+@endif
 @endsection
