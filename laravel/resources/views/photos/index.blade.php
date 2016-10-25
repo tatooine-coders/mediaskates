f90596e4dd1bbedc77e63bce9cef3934a523ea7f
Corbiezorq <jee.c@infonie.fr>
2016-10-25 15:32:34 +0200

    amelioration css pagination supression cdn, correctifs

--- laravel/resources/views/photos/index.blade.php
+++ laravel/resources/views/photos/index.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts/simple')
 
-<!--CDN bootstrap - Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
 @section('content')
 @if(count($photos)>0)
 <div style="text-align: center">
