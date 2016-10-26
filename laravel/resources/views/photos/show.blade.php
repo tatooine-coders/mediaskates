ab81416a1d4eb8215192da206d2c895cbbc68465
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-10-26 19:35:39 +0200

    Added vote display

--- laravel/resources/views/photos/show.blade.php
+++ laravel/resources/views/photos/show.blade.php
@@ -16,35 +16,46 @@
 
     {{--affichage des commentaires--}}
     <div id="comments">
-    @if(count($photo->comments)>0)
+        @if(count($photo->comments)>0)
         <h2>Commentaire(s)</h2>
         @foreach($photo->comments as $comment)
-                Le {{ $comment->created_at }}</br>
+        Le {{ $comment->created_at }}</br>
 
-                {{ $comment->user->pseudo }} à ecrit : </br>
-                {{ $comment->text }}</br>
-                </br>
+        {{ $comment->user->pseudo }} à ecrit : </br>
+        {{ $comment->text }}</br>
+        </br>
 
         @endforeach
 
-    @endif
+        @endif
 
-    {{--affichage ajout commentaire--}}
-    @if(Laratrust::hasRole('member'))
+        {{--affichage ajout commentaire--}}
+        @if(Laratrust::hasRole('member'))
 
-            <h2>Ajouter un Commentaire</h2>
+        <h2>Ajouter un Commentaire</h2>
 
-            {!! Form::open([
-            'method' => 'POST',
-            'route' => ['user.comment.store']
-            ]) !!}
+        {!! Form::open([
+        'method' => 'POST',
+        'route' => ['user.comment.store']
+        ]) !!}
 
-            {{ Form::hidden('photo_id',  $photo->id  ) }}
-            {{ Form::textarea('text', 'votre commentaire') }}<br/>
+        {{ Form::hidden('photo_id',  $photo->id  ) }}
+        {{ Form::textarea('text', 'votre commentaire') }}<br/>
 
-            {!! Form::submit('Ajouter un commentaire') !!}
-            {!! Form::close() !!}
+        {!! Form::submit('Ajouter un commentaire') !!}
+        {!! Form::close() !!}
+        <div id="votes" style="background-color:#FFF;">
+            <a href="javascript:void(0);" onclick="$(this).find('form').submit();" class="btn btn-info grave" title="Supprimer">
+                <i class="fa fa-fw fa-arrow-circle-o-up fa-2x" style="color:green;cursor:pointer;"></i>
+                {!! Form::open(['route'=>['user.vote.do_vote', 'photo_id'=> $photo->id], 'method'=>'POST']) !!}
+                {!! Form::close() !!}
+            </a>
+
+            {{ $photo->votes_count }}
+
+        </div>
+        @endif
     </div>
-    @endif
+
 </div>
 @endsection
