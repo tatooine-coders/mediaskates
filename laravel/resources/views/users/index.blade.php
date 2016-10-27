4cfbd21a882fa97598cad8c442913fa27c39f121
Vincent Lecourt <lecourt.vincent@free.fr>
2016-10-27 14:32:10 +0200

    Révision

--- laravel/resources/views/users/index.blade.php
+++ laravel/resources/views/users/index.blade.php
@@ -4,18 +4,21 @@
 @if(count($users)>0)
 @foreach($users as $user)
 <div class="utils">
-<h2>
-    [{{ link_to('user/'.$user->id, 'Voir')}}]
-    <!-- [{{ link_to('user/'.$user->id.'/edit', 'Editer')}}] -->
-    - {{{ $user->pseudo }}}
-</h2>
+    <h2>
+        <!--[{{ link_to('user/'.$user->id, 'Voir')}}]
+         [{{ link_to('user/'.$user->id.'/edit', 'Editer')}}] -->
+        {{{ $user->pseudo }}}
+    </h2>
 
 
     <ul>
-        <img class="avatar" src="{{ asset(PROFILE_PICS_FOLDER . (!empty($user->profile_pic)?$user->profile_pic:DEFAULT_PROFILE_PIC)) }}"/>
-        <li>Prénom : {{{ $user->first_name }}}</li> 
-        <li>Nom : {{{ $user->last_name }}}</li>
-        <li>Pseudo : {{{ $user->pseudo }}}</li>
+           
+                <a href="{{('user/'.$user->id)}}">
+                <img class="avatar" src="{{ asset(PROFILE_PICS_FOLDER . (!empty($user->profile_pic)?$user->profile_pic:DEFAULT_PROFILE_PIC)) }}"/>
+            </a>
+            <li>Prénom : {{{ $user->first_name }}}</li> 
+            <li>Nom : {{{ $user->last_name }}}</li>
+            <li>Pseudo : {{{ $user->pseudo }}}</li>
     </ul>
 </div>
 
