dc631d964c6dac9fc8559a030e395e7f5af3378d
Corbiezorq <jee.c@infonie.fr>
2016-10-27 17:08:08 +0200

    vrfrzf

--- laravel/resources/views/users/show.blade.php
+++ laravel/resources/views/users/show.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts/simple')
-<<<<<<< HEAD
-=======
+
 @section('content')
 <section id="photog">
     <img class="pavatar" src="{{ asset(PROFILE_PICS_FOLDER . (!empty($user->profile_pic)?$user->profile_pic:DEFAULT_PROFILE_PIC)) }}"/>
@@ -24,25 +23,3 @@
         </ul>
         <div id="bandoRec"></div>
         <div id="bandoTri"></div>
->>>>>>> origin/Vincent
-
-@section('content')
-<h2>
-	[{{ link_to(route('user.index'), '< Liste')}}]
-	- {{{ $user->pseudo }}}</h2>
-<pre>
-{{{ $user->first_name }}}
-{{{ $user->last_name }}}
-{{{ $user->pseudo }}}
-{{{ $user->email }}}
-{{ $user->profile_pic }}
-{{ $user->active }}
-{{ $user->preferences }}
-{{{ $user->site_web }}}
-{{{ $user->facebook }}}
-{{{ $user->google }}}
-{{{ $user->twitter }}}
-{{{ $user->biography }}}
-
-</pre>
-@endsection
