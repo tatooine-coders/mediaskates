c645372a48f066eb35a4c9932481853a473008d2
Vincent Lecourt <lecourt.vincent@free.fr>
2016-10-27 08:02:45 +0200

    debut photographe

--- laravel/resources/views/users/index.blade.php
+++ laravel/resources/views/users/index.blade.php
@@ -3,21 +3,20 @@
 @section('content')
 @if(count($users)>0)
 @foreach($users as $user)
-
+<div class="utils">
 <h2>
     [{{ link_to('user/'.$user->id, 'Voir')}}]
-    [{{ link_to('user/'.$user->id.'/edit', 'Editer')}}]
+    <!-- [{{ link_to('user/'.$user->id.'/edit', 'Editer')}}] -->
     - {{{ $user->pseudo }}}
 </h2>
 
-<div class="utils">
+
     <ul>
-        <img class="avatar" src="{{ asset(PROFILE_PICS_FOLDER . (!empty($user->profile_pic)?$user->profile_pic:DEFAULT_PROFILE_PIC)) }}"/></li>
+        <img class="avatar" src="{{ asset(PROFILE_PICS_FOLDER . (!empty($user->profile_pic)?$user->profile_pic:DEFAULT_PROFILE_PIC)) }}"/>
         <li>Prénom : {{{ $user->first_name }}}</li> 
         <li>Nom : {{{ $user->last_name }}}</li>
         <li>Pseudo : {{{ $user->pseudo }}}</li>
-        </div></li>
-     </ul>
+    </ul>
 </div>
 
 @endforeach
--- laravel/resources/views/users/show.blade.php
+++ laravel/resources/views/users/show.blade.php
@@ -1,22 +1,29 @@
 @extends('layouts/simple')
-
 @section('content')
-<h2>
-	[{{ link_to(route('user.index'), '< Liste')}}]
-	- {{{ $user->pseudo }}}</h2>
-<pre>
-{{{ $user->first_name }}}
-{{{ $user->last_name }}}
-{{{ $user->pseudo }}}
-{{{ $user->email }}}
-{{ $user->profile_pic }}
-{{ $user->active }}
-{{ $user->preferences }}
-{{{ $user->site_web }}}
-{{{ $user->facebook }}}
-{{{ $user->google }}}
-{{{ $user->twitter }}}
-{{{ $user->biography }}}
-
-</pre>
+<section id="photog">
+    <ul>
+        <li><h2>[{{ link_to(route('user.index'), '< Liste')}}]- {{{ $user->pseudo }}}</h2></li>
+        <img class="pavatar" src="{{ asset(PROFILE_PICS_FOLDER . (!empty($user->profile_pic)?$user->profile_pic:DEFAULT_PROFILE_PIC)) }}"/><div id='mask'>
+            <li>{{{ $user->first_name }}}
+        {{{ $user->last_name }}}
+        {{{ $user->pseudo }}}
+        {{{ $user->email }}}
+        {{ $user->profile_pic }}
+        {{ $user->active }}
+        {{ $user->preferences }}
+        {{{ $user->site_web }}}
+        {{{ $user->facebook }}}
+        {{{ $user->google }}}
+        {{{ $user->twitter }}}
+        {{{ $user->biography }}}</li>
+    </ul>    
+        <div id="bandoRec"></div>
+        <div id="bandoTri"></div>
+      
+    </div>
+    <div id='diapo'>
+        <img src="{{ asset('images/sources/roller.jpg') }}" alt>
+    </div>     
+   
+</section>
 @endsection
