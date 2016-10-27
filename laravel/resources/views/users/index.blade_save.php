8a477f41442f14b3edd099c2bf7ad47d8d5271ae
Vincent Lecourt <lecourt.vincent@free.fr>
2016-10-27 12:21:17 +0200

    vue Photographe

---
+++ laravel/resources/views/users/index.blade_save.php
@@ -0,0 +1,30 @@
+@extends('layouts/simple')
+
+@section('content')
+@if(count($users)>0)
+@foreach($users as $user)
+<h2>
+    [{{ link_to('user/'.$user->id, 'Voir')}}]
+    [{{ link_to('user/'.$user->id.'/edit', 'Editer')}}]
+    - {{{ $user->pseudo }}}
+</h2>
+<pre>
+{{{ $user->first_name }}}
+{{{ $user->last_name }}}
+{{{ $user->pseudo }}}
+{{{ $user->email }}}
+{{{ $user->profile_pic }}}
+{{{ $user->active }}}
+{{{ $user->preferences }}}
+{{{ $user->site_web }}}
+{{{ $user->facebook }}}
+{{{ $user->google }}}
+{{{ $user->twitter }}}
+{{{ $user->biography }}}
+
+</pre>
+@endforeach
+@else
+Pas d'utilisateurs.
+@endif
+@endsection
