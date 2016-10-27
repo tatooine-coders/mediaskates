ab6acb4a7068849fb120694831f8604d4c82a819
Vincent Lecourt <lecourt.vincent@free.fr>
2016-10-27 16:57:16 +0200

    maj photographe icons de liens externes caché si vide augmentaion taille de police les mentions legales

--- laravel/resources/views/users/show.blade.php
+++ laravel/resources/views/users/show.blade.php
@@ -4,7 +4,7 @@
     <img class="pavatar" src="{{ asset(PROFILE_PICS_FOLDER . (!empty($user->profile_pic)?$user->profile_pic:DEFAULT_PROFILE_PIC)) }}"/>
     <div id="mask">
         <ul>
-            <h2>[{{ link_to(route('user.index'), '< Liste')}}] - {{ $user->pseudo }}</h2>
+             <a href="{{ route('user.index') }}"><h2><i class="fa fa-fw fa-chevron-left"></i>{{   $user->pseudo }}</h2></a>
             <ul>
                 <li>Prénom : {{{ $user->first_name }}}</li>
                 <li>Nom : {{{ $user->last_name }}}</li>
@@ -12,10 +12,28 @@
                 <!-- <li>{{{ $user->email }}}</li> -->
                 <!-- <li>{{ $user->active }}</li> -->
                 <!-- <li>{{ $user->preferences }}</li> -->
-                <li><a href="{{ $user->site_web }}"><i class="fa fa-fw fa-globe"></i></a>
-                <a href="{{ $user->facebook }}"><i class="fa fa-fw fa-facebook"></i></a>
-                <a href="{{ $user->google }}"><i class="fa fa-fw fa-google"></i></a>
-                <a href="{{ $user->twitter }}"><i class="fa fa-fw fa-twitter"></i></a></li>
+                <li>
+                    @if(!empty($user->site_web))
+                    <a href="{{ $user->site_web }}"><i class="fa fa-fw fa-globe"></i></a>
+                    @else
+                    <i class="fa fa-fw fa-2x fa-fw"></i>
+                    @endif
+                    @if(!empty($user->facebook))   
+                    <a href="{{ $user->facebook }}"><i class="fa fa-fw fa-facebook"></i></a>
+                    @else
+                    <i class="fa fa-fw fa-2x fa-fw"></i>
+                    @endif
+                    @if(!empty($user->google))
+                    <a href="{{ $user->google }}"><i class="fa fa-fw fa-google"></i></a>
+                    @else
+                    <i class="fa fa-fw fa-2x fa-fw"></i>
+                    @endif
+                    @if(!empty($user->twitter))
+                    <a href="{{ $user->twitter }}"><i class="fa fa-fw fa-twitter"></i></a>
+                    @else
+                    <i class="fa fa-fw fa-2x fa-fw"></i>
+                    @endif
+                </li>
                 <br>
             </ul>
             <p>{!!nl2br($user->biography)!!}</p>
